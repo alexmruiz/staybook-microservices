6797c72d12b97afd077eaf0e28161a8a3d25dd58
Alejandro Moya Ruiz <alexmyruiz@gmail.com>
2026-08-24 10:10:24 +0200

    chore: clean up ReviewDTO by removing unnecessary whitespace

--- reviews-service/src/main/java/com/hotelsbook/reviews/dto/ReviewDTO.java
+++ reviews-service/src/main/java/com/hotelsbook/reviews/dto/ReviewDTO.java
@@ -5,34 +5,24 @@ public class ReviewDTO {
     private Long hotelId;
     private Double averageCalification;
 
-
     public ReviewDTO(Long hotelId, Double averageCalification) {
         this.hotelId = hotelId;
         this.averageCalification = averageCalification;
     }
 
-
     public Long getHotelId() {
         return hotelId;
     }
-
 
     public void setHotelId(Long hotelId) {
         this.hotelId = hotelId;
     }
 
-
     public Double getAverageCalification() {
         return averageCalification;
     }
 
-
     public void setAverageCalification(Double averageCalification) {
         this.averageCalification = averageCalification;
     }
-
-    
-
-    
-
 }
