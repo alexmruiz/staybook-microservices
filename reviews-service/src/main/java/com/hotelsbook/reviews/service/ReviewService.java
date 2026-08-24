ca516ab5a5d3f9d7b01ae350a74c134840a7c8bb
Alejandro Moya Ruiz <alexmyruiz@gmail.com>
2026-08-24 14:05:09 +0200

    chore: refactor ReviewService to improve structure and add update and delete methods

--- reviews-service/src/main/java/com/hotelsbook/reviews/service/ReviewService.java
+++ reviews-service/src/main/java/com/hotelsbook/reviews/service/ReviewService.java
@@ -1,35 +1,91 @@
 package com.hotelsbook.reviews.service;
 
 import java.util.List;
-import java.util.stream.Collectors;
 
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
-import com.hotelsbook.reviews.dto.ReviewDTO;
+import com.hotelsbook.reviews.dto.ReviewRequestDto;
+import com.hotelsbook.reviews.dto.ReviewResponseDto;
+import com.hotelsbook.reviews.entity.ReviewEntity;
+import com.hotelsbook.reviews.mapper.ReviewMapper;
 import com.hotelsbook.reviews.repository.ReviewRepository;
 
 @Service
 public class ReviewService {
 
-    @Autowired
-    private ReviewRepository reviewRepository;
+    
+    private final ReviewRepository reviewRepository;
+
+    private final ReviewMapper reviewMapper;
+
+    public ReviewService(ReviewRepository reviewRepository, ReviewMapper reviewMapper) {
+        this.reviewRepository = reviewRepository;
+        this.reviewMapper = reviewMapper;
+    }
 
     /**
-     * Guarda en la lista los valores recibidos
-     * 
-     * @param hotelIds
+     * Create
+     * @param request ReviewRequestDto
+     * @return ReviewResponseDto
+     */
+    public ReviewResponseDto create(ReviewRequestDto request) 
+    {
+        ReviewEntity reviewEntity = reviewMapper.toEntity(request);
+        ReviewEntity saved = reviewRepository.save(reviewEntity);
+        return reviewMapper.toResponseDto(saved);
+    }
+
+    /**
+     * find all
+     * @return List ReviewResponseDto
+     */
+    public List<ReviewResponseDto> findAll() 
+    {
+        return reviewRepository.findAll()
+            .stream()
+            .map(reviewMapper::toResponseDto)
+            .toList();
+    }
+    
+    /**
+     * find by id
+     * @param id
      * @return
      */
-    public List<ReviewDTO> getAverageCalifications(String hotelIds) {
+    public ReviewResponseDto findById(Long id)
+    {
+        ReviewEntity review = reviewRepository.findById(id).orElseThrow(()-> new RuntimeException("Reseña no encontrada con Id: " + id));
 
-        List<Object[]> results = reviewRepository.findAverageCalificationByHotel(hotelIds);
+        return reviewMapper.toResponseDto(review);
+    }
 
-        return results.stream().map(result -> new ReviewDTO(
-                ((Number) result[1]).longValue(), // hotelId
-                ((Number) result[2]).doubleValue()// averageCalification
-        )).collect(Collectors.toList());
+    /**
+     * Update a review
+     * @param Long id
+     * @param ReviewRequestDto request
+     * @return ReviewResponseDto
+     */
+    public ReviewResponseDto update(Long id, ReviewRequestDto request)
+    {
+        ReviewEntity existReview = reviewRepository.findById(id).orElseThrow(() -> new RuntimeException("Reseña no encontrada"));
 
+        existReview.setAverageCalification(request.getAverageCalification());
+        existReview.setHotelId(request.getHotelId());
+
+        ReviewEntity update = reviewRepository.save(existReview);
+
+        return reviewMapper.toResponseDto(update);
+    }
+    /**
+     * Delete by id
+     * @param id
+     */
+    public void delete(Long hotelId)
+    {
+        if (!reviewRepository.existsByHotelId(hotelId))
+            throw new RuntimeException("No se puede eliminar. Reseña no encontrada");
+
+        reviewRepository.deleteById(hotelId);
     }
 
 }
