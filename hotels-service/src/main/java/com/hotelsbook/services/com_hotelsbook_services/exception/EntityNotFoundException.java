27d011a6fcd9e81cfa7604f17a494c21bd205d44
Alejandro Moya Ruiz <alexmyruiz@gmail.com>
2026-09-02 12:40:49 +0200

    feat: add GlobalExceptionHandler to manage exceptions and improve error handling

---
+++ hotels-service/src/main/java/com/hotelsbook/services/com_hotelsbook_services/exception/EntityNotFoundException.java
@@ -0,0 +1,9 @@
+package com.hotelsbook.services.com_hotelsbook_services.exception;
+
+public class EntityNotFoundException extends RuntimeException {
+
+    public EntityNotFoundException(String message) {
+        super(message);
+    }
+
+}
