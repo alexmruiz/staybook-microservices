56b168ab3802d502f3c60e4ffcb955063687bafe
Alejandro Moya Ruiz <alexmyruiz@gmail.com>
2026-09-15 13:27:05 +0200

    refactor: remove Testcontainers setup and use AutoConfigureTestDatabase for testing

--- hotels-service/src/test/java/com/hotelsbook/services/com_hotelsbook_services/ComHotelsbookServicesApplicationTests.java
+++ hotels-service/src/test/java/com/hotelsbook/services/com_hotelsbook_services/ComHotelsbookServicesApplicationTests.java
@@ -1,30 +1,11 @@
 package com.hotelsbook.services.com_hotelsbook_services;
-
 import org.junit.jupiter.api.Test;
-import org.testcontainers.containers.PostgreSQLContainer;
-import org.testcontainers.junit.jupiter.Container;
-import org.testcontainers.junit.jupiter.Testcontainers;
+import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.test.context.DynamicPropertyRegistry;
-import org.springframework.test.context.DynamicPropertySource;
 
 @SpringBootTest
-@Testcontainers
+@AutoConfigureTestDatabase (replace = AutoConfigureTestDatabase.Replace.ANY)
 class ComHotelsbookServicesApplicationTests {
-
-    @Container
-    static PostgreSQLContainer<?> postgres = new PostgreSQLContainer<>("postgres:15")
-            .withDatabaseName("testdb")
-            .withUsername("test")
-            .withPassword("test");
-
-    @DynamicPropertySource
-    static void overrideProperties(DynamicPropertyRegistry registry) {
-        registry.add("spring.datasource.url", postgres::getJdbcUrl);
-        registry.add("spring.datasource.username", postgres::getUsername);
-        registry.add("spring.datasource.password", postgres::getPassword);
-    }
-
     @Test
     void contextLoads() { }
 }
