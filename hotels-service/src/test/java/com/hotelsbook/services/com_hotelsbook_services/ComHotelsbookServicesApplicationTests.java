557ca330602ded08261fdcf849ea8c2188c91959
Alejandro Moya Ruiz <alexmyruiz@gmail.com>
2026-09-15 13:13:45 +0200

    refactor: add Testcontainers support for PostgreSQL in tests and update dependencies in pom.xml

--- hotels-service/src/test/java/com/hotelsbook/services/com_hotelsbook_services/ComHotelsbookServicesApplicationTests.java
+++ hotels-service/src/test/java/com/hotelsbook/services/com_hotelsbook_services/ComHotelsbookServicesApplicationTests.java
@@ -1,13 +1,30 @@
 package com.hotelsbook.services.com_hotelsbook_services;
 
 import org.junit.jupiter.api.Test;
+import org.testcontainers.containers.PostgreSQLContainer;
+import org.testcontainers.junit.jupiter.Container;
+import org.testcontainers.junit.jupiter.Testcontainers;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.DynamicPropertyRegistry;
+import org.springframework.test.context.DynamicPropertySource;
 
 @SpringBootTest
+@Testcontainers
 class ComHotelsbookServicesApplicationTests {
 
-	@Test
-	void contextLoads() {
-	}
+    @Container
+    static PostgreSQLContainer<?> postgres = new PostgreSQLContainer<>("postgres:15")
+            .withDatabaseName("testdb")
+            .withUsername("test")
+            .withPassword("test");
 
+    @DynamicPropertySource
+    static void overrideProperties(DynamicPropertyRegistry registry) {
+        registry.add("spring.datasource.url", postgres::getJdbcUrl);
+        registry.add("spring.datasource.username", postgres::getUsername);
+        registry.add("spring.datasource.password", postgres::getPassword);
+    }
+
+    @Test
+    void contextLoads() { }
 }
